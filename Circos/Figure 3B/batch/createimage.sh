18fd9fcbf5c0f5032150f3cbbf8481433db92b94
maximorebeca <148349366+maximorebeca@users.noreply.github.com>
2023-10-18 10:17:58 -0300

    Add files via upload

---
+++ Circos/Figure 3B/batch/createimage.sh
@@ -0,0 +1,7 @@
+# Set PERL, TABLEVIEWER_DIR, CIRCOS_DIR and WORKING_DIR variables based on you system.
+cd WORKING_DIR
+$PERL $TABLEVIEWER_DIR/bin/parse-table -conf etc/parse-table.conf -file uploads/table.txt -max_col_num 40 -max_row_num 40 -segment_order=ascii,size_desc -placement_order=row,col -interpolate_type count -row_order_col -use_row_order_col -row_color_col -use_row_color_col -color_source row -transparency 1 -fade_transparency 0 -ribbon_layer_order=size_asc > data/parsed.txt 2> WORKING_DIR/data/stderr.txt
+cat data/parsed.txt | $TABLEVIEWER_DIR/bin/make-conf -dir data
+$PERL $CIRCOS_DIR/bin/circos -param random_string=lnfmjff -conf etc/circos.conf
+cd WORKING_DIR; PERL CIRCOS_DIR/bin/circos -param random_string=lnfmjff -conf WORKING_DIR/etc/circos.conf 2>&1 > WORKING_DIR/results/report.txt
+tar cvfz circos-tableviewer-lnfmjff.tar.gz *
